7f843590382cdfda59b01bbbc6346c9c796b024e
Matthew Kokotovich <mkokotovich@gmail.com>
2016-11-08 16:11:49 -0600

    Removed namespace, fixed background thread

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 from flask import Flask, render_template, session, request, url_for
 from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room, \
     close_room, rooms, disconnect
-import base64
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
 async_mode = "eventlet"
 
+page_title = "Control the Lights"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
@@ -19,15 +19,18 @@ def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
+        # Do the work of reading images and uploading them to the cloud here
+        # Then brodcast out a new image url
+        # See http://cloudinary.com/documentation/django_image_upload#server_side_upload
         socketio.sleep(10)
         count += 1
         socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count}, namespace='/test')
+                      {'data': 'Server generated event', 'count': count}, namespace='')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+    return render_template('index.html', async_mode=socketio.async_mode, page_title=page_title)
 
 
 class MyNamespace(Namespace):
@@ -41,9 +44,15 @@ class MyNamespace(Namespace):
         emit('picture_update',
             {'count': session['receive_count'], 'source': "static/" + img_src})
 
+    def on_connect(self):
+        global thread
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
 
-socketio.on_namespace(MyNamespace('/test'))
+
+socketio.on_namespace(MyNamespace(''))
 
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host="0.0.0.0", port=8080)
+
