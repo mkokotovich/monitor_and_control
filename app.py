966113e9c27d5988dd0542ecd72952a606ed7a47
Matthew Kokotovich <mkokotovich@gmail.com>
2016-11-08 23:53:00 -0600

    Capture image from camera, upload images to cloud, pass to the client

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 from flask import Flask, render_template, session, request, url_for
 from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room, \
     close_room, rooms, disconnect
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
+import urllib
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -13,19 +16,33 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
+camera_server = "http://192.168.86.106:8080"
+image_number = 0
+filename = "images/image_latest.jpg"
+
+def refresh_image(filename):
+    # TODO: limit refresh to a certain rate
+    # Grab the latest frame from the camera
+    urllib.urlretrieve("{}{}".format(camera_server, "/photo.jpg"), filename)
+    # Upload the image to the cloud
+    upload_result = upload(filename)
+    if upload_result:
+        image_url = upload_result['url']
+    else:
+        image_url = None
+    # Return the url of the new image
+    return image_url
 
 
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
-        # Do the work of reading images and uploading them to the cloud here
-        # Then brodcast out a new image url
-        # See http://cloudinary.com/documentation/django_image_upload#server_side_upload
-        socketio.sleep(10)
-        count += 1
-        socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count}, namespace='')
+        socketio.sleep(15)
+        img_url = refresh_image(filename)
+        socketio.emit('picture_update',
+            {'source': img_url},
+            namespace='')
 
 
 @app.route('/')
@@ -36,15 +53,14 @@ def index():
 class MyNamespace(Namespace):
 
     def on_update_request(self):
-        session['receive_count'] = session.get('receive_count', 0) + 1
-        if session['receive_count'] % 2 == 0:
-            img_src = "lights.jpg"
-        else:
-            img_src = "lights2.jpg"
+        img_url = refresh_image(filename) #add force=True
         emit('picture_update',
-            {'count': session['receive_count'], 'source': "static/" + img_src})
+            {'source': img_url})
 
     def on_connect(self):
+        img_url = refresh_image(filename)
+        emit('picture_update',
+            {'source': img_url})
         global thread
         if thread is None:
             thread = socketio.start_background_task(target=background_thread)
